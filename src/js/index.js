f9c29cbfca68f64c7c6782e773b6f1865dd869ce
andy <andy.knapp@thinkcompany.com>
2024-03-24 21:54:05 -0400

    starts main animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 import { executeWhenReady, modalFocusTrap } from "./_utils.js";
 
+const ioHome = () => {
+  const container = document.querySelector(".io-hero");
+  const animationClass = "io-is-animating";
+
+  const handleHeroAnimation = () => {
+    container.classList.toggle(animationClass);
+  };
+
+  container.addEventListener("click", handleHeroAnimation);
+};
+
+
 executeWhenReady(() => {
-  console.log('dom loaded');
+  ioHome();
 });
