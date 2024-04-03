cf05240833b7a8c36a74c12701e2550d8f41f69b
andy <andy.knapp@thinkcompany.com>
2024-04-03 09:12:35 -0400

    animation updates

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,41 @@ import { executeWhenReady, modalFocusTrap } from "./_utils.js";
 
 const ioHome = () => {
   const container = document.querySelector(".io-hero");
-  const animationClass = "io-is-animating";
+  const outerRing = container.querySelector(".ring-outer");
+  const animationClassA = "io-is-animating-a";
+  const animationClassB = "io-is-animating-b";
+  let isAnimating = false;
+  let animationStep = 0;
 
-  const handleHeroAnimation = () => {
-    container.classList.toggle(animationClass);
+  const handleHeroAnimationClick = () => {
+    if (!isAnimating && animationStep === 0) {
+      isAnimating = true;
+      container.classList.add(animationClassA);
+      animationStep = 1;
+    } else if (!isAnimating && animationStep === 1) {
+      isAnimating = true;
+      container.classList.add(animationClassB);
+      animationStep = 0;
+    }
   };
 
-  container.addEventListener("click", handleHeroAnimation);
+  const handleAnimationEnd = (event) => {
+    if (event.animationName === "ringUp") {
+      console.log("animationend");
+      container.classList.remove(animationClassA);
+      isAnimating = false;
+
+    } else if (event.animationName === "ringUpBack") {
+      container.classList.remove(animationClassB);
+      isAnimating = false;
+    }
+  };
+
+  container.addEventListener("click", handleHeroAnimationClick);
+  outerRing.addEventListener("animationend", (event) => {
+    handleAnimationEnd(event);
+  });
+
 };
 
 
